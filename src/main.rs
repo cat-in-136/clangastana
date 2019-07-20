4078e167cb6bffd18ddfec5fb9bb956c05da056d
cat_in_136 <cat.in.136+github@gmail.com>
2019-07-20 21:14:14 +0900

    introduce output option

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ use clang::{Clang, Entity, EntityVisitResult, Index, TranslationUnit};
 use clap::{App, AppSettings, Arg};
 use std::error::Error;
 use std::fmt::{Display, Formatter};
-use std::io::Write;
+use std::fs::File;
+use std::io::Error as IoError;
+use std::io::{BufWriter, Write};
 use xml::writer::Error as XmlError;
 use xml::writer::XmlEvent;
 use xml::{EmitterConfig, EventWriter};
@@ -17,6 +19,7 @@ extern crate clap;
 pub enum AstXmlError {
     Clang(SourceError),
     Xml(XmlError),
+    Io(IoError),
 }
 
 impl Display for AstXmlError {
@@ -24,6 +27,7 @@ impl Display for AstXmlError {
         match self {
             AstXmlError::Clang(e) => write!(f, "${:?}", e),
             AstXmlError::Xml(e) => write!(f, "${:?}", e),
+            AstXmlError::Io(e) => write!(f, "${:?}", e),
         }
     }
 }
@@ -40,9 +44,15 @@ impl From<SourceError> for AstXmlError {
     }
 }
 
+impl From<IoError> for AstXmlError {
+    fn from(e: IoError) -> Self {
+        AstXmlError::Io(e)
+    }
+}
+
 impl Error for AstXmlError {}
 
-pub fn create_start_xml_event_from_entry<W: Write>(
+fn create_start_xml_event_from_entry<W: Write>(
     entry: Entity,
     writer: &mut EventWriter<W>,
 ) -> Result<(), XmlError> {
@@ -82,29 +92,15 @@ pub fn create_start_xml_event_from_entry<W: Write>(
     writer.write(XmlEvent::from(elem))
 }
 
-pub fn create_end_xml_event_from_entry<W: Write>(
-    writer: &mut EventWriter<W>,
-) -> Result<(), XmlError> {
+fn create_end_xml_event_from_entry<W: Write>(writer: &mut EventWriter<W>) -> Result<(), XmlError> {
     let elem = XmlEvent::end_element();
     writer.write(XmlEvent::from(elem))
 }
 
-pub fn parse_translation_unit(
-    source_file_path: &str,
-    arguments: &[&str],
+fn parse_translation_unit<W: Write>(
+    tu: TranslationUnit,
+    mut writer: &mut EventWriter<W>,
 ) -> Result<(), AstXmlError> {
-    let mut writer = EmitterConfig::new()
-        .perform_indent(true)
-        .create_writer(std::io::stdout());
-
-    let clang = Clang::new().unwrap();
-    let index = Index::new(&clang, false, false);
-
-    let tu: TranslationUnit = {
-        let mut parser = index.parser(source_file_path);
-        parser.arguments(arguments);
-        parser.parse().or_else(|e| Err(AstXmlError::Clang(e)))?
-    };
     let root_entity = tu.get_entity();
 
     let mut breadcrumbs = vec![root_entity];
@@ -141,17 +137,41 @@ pub fn parse_translation_unit(
     Ok(())
 }
 
+pub fn process_astxml(
+    source_file_path: &str,
+    arguments: &[&str],
+    output_file_path: Option<&str>,
+) -> Result<(), AstXmlError> {
+    let clang = Clang::new().unwrap();
+    let index = Index::new(&clang, false, false);
+    let tu: TranslationUnit = {
+        let mut parser = index.parser(source_file_path);
+        parser.arguments(arguments);
+        parser.parse().or_else(|e| Err(AstXmlError::Clang(e)))?
+    };
+
+    let output = if let Some(path) = output_file_path {
+        Box::new(File::create(path)?) as Box<Write>
+    } else {
+        Box::new(std::io::stdout()) as Box<Write>
+    };
+    let mut writer = EmitterConfig::new()
+        .perform_indent(true)
+        .create_writer(BufWriter::new(output));
+
+    parse_translation_unit(tu, &mut writer)
+}
+
 fn main() {
     let matches = (app_from_crate!() as App)
         .setting(AppSettings::TrailingVarArg)
-        //        .arg(
-        //            Arg::with_name("output")
-        //                .short("o")
-        //                .long("output")
-        //                .value_name("OUTPUT")
-        //                .help("Sets the XML output file")
-        //                .takes_value(true),
-        //        )
+        .arg(
+            Arg::with_name("OUTPUT")
+                .short("o")
+                .long("output")
+                .help("Sets the XML output file")
+                .takes_value(true),
+        )
         .arg(
             Arg::with_name("INPUT")
                 .help("input file (source file) and trailing compiler arguments")
@@ -160,12 +180,12 @@ fn main() {
         )
         .get_matches();
 
+    let output = matches.value_of("OUTPUT");
     let mut input = matches.values_of("INPUT").unwrap().collect::<Vec<_>>();
     let source_file_path = input.pop().unwrap();
     let arguments = &input[..];
-    //let output = matches.value_of("OUTPUT");
 
-    match parse_translation_unit(source_file_path, arguments) {
+    match process_astxml(source_file_path, arguments, output) {
         Ok(_) => (),
         Err(_) => {
             eprintln!("Error");
